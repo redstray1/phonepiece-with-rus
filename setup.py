f576daf7c0da70f76939902e91d5eb6d5b1f4f98
Grigory Afanasenko <97543796+redstray1@users.noreply.github.com>
2025-05-10 16:41:41 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ requirements=[r.strip() for r in open("requirements.txt").readlines()]
 
 setup(
    name='phonepiece',
-   version='1.4.2',
+   version='1.0.0',
    description='a multilingual phone tokenizer',
    author='Xinjian Li',
    author_email='xinjianl@cs.cmu.edu',
-   url="https://github.com/xinjli/phonepiece",
+   url="https://github.com/redstray1/phonepiece-with-rus",
    packages=find_packages(),
    install_requires=requirements,
    package_data={'': ['*.csv', '*.tsv', 'tree.txt', 'pinyin2ipa.txt', 'model/*']},
